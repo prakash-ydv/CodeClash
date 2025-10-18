4d1f4f1721cc50ab683bfb57db24b07ab9ae37aa
Prakash Kumar <prakashyadav9576@gmail.com>
2025-10-18 22:29:23 +0530

    setup routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProblemsPage from "./pages/ProblemsPage";
+import LoginPage from "./pages/LoginPage";
+import LeaderBoardPage from "./pages/LeaderBoardPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/problems" element={<ProblemsPage />} />
+        <Route path="/leaderboard" element={<LeaderBoardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
